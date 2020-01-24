06dfb75c73d8571e827ac316eca3051d0c9d3ebd
Joao Pinto <joao.pinto@vshn.ch>
2020-01-24 12:57:12 +0100

    Add sample mod_security conf

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${HAPROXY_IP} ${HAPROXY_PRIVATE_PORT}' < /etc/nginx/conf.d/mysite.conf.template > /etc/nginx/conf.d/mysite.conf
+envsubst '${NGINX_PORT} ${HAPROXY_IP} ${HAPROXY_PRIVATE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
